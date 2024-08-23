fec642795f90db1b0371e904213117ed0c51ced3
Beatriz Newman <102912868+beatriznewman@users.noreply.github.com>
2024-08-23 16:05:16 -0300

    Update interface_inicial.py

--- interface_inicial.py
+++ interface_inicial.py
@@ -4,80 +4,71 @@ import random
 
 # Variáveis para armazenar os estados e movimentos
 historico_movimentos = []
-estado_embaralhado_inicial = []  # Adiciona uma variável para armazenar o estado inicial embaralhado
+estado_embaralhado_inicial = []
 
 # Função para verificar o estado final do jogo
 def solução(next_config):
     return next_config == [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
+# Função para mover a peça
 def mover_peça(row, col, current_puzzle):
-    empty_pos = current_puzzle.index(0)  # Encontra a posição do espaço vazio no quebra-cabeça
-    espaco_vazio, empty_col = divmod(empty_pos, 3)  # Converte a posição linear do espaço vazio para coordenadas de linha e coluna na grade 3x3
+    empty_pos = current_puzzle.index(0)
+    espaco_vazio, empty_col = divmod(empty_pos, 3)
 
-    if abs(espaco_vazio - row) + abs(empty_col - col) == 1:  # Verifica se a peça clicada está adjacente ao espaço vazio
-        # Cria uma cópia do puzzle atual para não modificar o original
+    if abs(espaco_vazio - row) + abs(empty_col - col) == 1:
         next_config = current_puzzle.copy()
-
-        # Executa a troca na cópia
         new_pos = row * 3 + col 
         next_config[empty_pos], next_config[new_pos] = next_config[new_pos], next_config[empty_pos]
 
-        # Armazena o estado atual e o movimento realizado antes da troca
         estado_antes = [current_puzzle[i:i + 3] for i in range(0, len(current_puzzle), 3)] 
         movimento = f"Mover espaço vazio de ({espaco_vazio}, {empty_col}) para ({row}, {col})"
         historico_movimentos.append((estado_antes, movimento))
 
-        update_buttons(next_config)  
-
-        if solução(next_config):
-            messagebox.showinfo("8 Puzzle", "Parabéns, você ganhou!!!")  
-            print_historico_movimentos()
-            print(puzzle)
-
-        return next_config  # Retorna a nova configuração
-
-    return current_puzzle  # Retorna o puzzle atual se a peça não puder ser movida
+        return next_config
+    return current_puzzle
 
 # Função para atualizar a interface com o estado atual do quebra-cabeça
 def update_buttons(current_puzzle):
-    for i, button in enumerate(buttons):  # Itera sobre cada botão
-        if current_puzzle[i] == 0:  # Se o valor do quebra-cabeça é 0 (espaço vazio)
-            button.config(text="", state=tk.DISABLED)  # Desativa o botão e limpa o texto
+    for i, button in enumerate(buttons):
+        if current_puzzle[i] == 0:
+            button.config(text="", state=tk.DISABLED)
         else:
-            button.config(text=str(current_puzzle[i]), state=tk.NORMAL)  # Atualiza o texto do botão com o número da peça
+            button.config(text=str(current_puzzle[i]), state=tk.NORMAL)
 
 # Função para embaralhar o quebra-cabeça
 def embaralhar_puzzle():
-    global estado_embaralhado_inicial, historico_movimentos
-    historico_movimentos.clear()  # Limpa o histórico de movimentos ao reiniciar o jogo
+    global estado_embaralhado_inicial, historico_movimentos, current_state
+    historico_movimentos.clear()
     while True:
         estado_inicial = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        random.shuffle(estado_inicial)  # Embaralha a lista de peças
-        if é_resolvível(estado_inicial):  # Verifica se a configuração embaralhada é resolvível
+        random.shuffle(estado_inicial)
+        if é_resolvível(estado_inicial):
             global puzzle
             puzzle = estado_inicial
-            estado_embaralhado_inicial = estado_inicial.copy()  # Armazena o estado embaralhado inicial
-            break  # Sai do loop se a configuração for resolvível
+            current_state = puzzle.copy()
+            estado_embaralhado_inicial = estado_inicial.copy()
+            break
 
 # Função para verificar se o quebra-cabeça é resolvível
 def é_resolvível(puzzle):
     inversões = 0
-    for i in range(len(puzzle)):  # Itera sobre cada peça
-        for j in range(i + 1, len(puzzle)):  # Compara com as peças subsequentes
-            if puzzle[i] != 0 and puzzle[j] != 0 and puzzle[i] > puzzle[j]:  # Conta inversões
+    for i in range(len(puzzle)):
+        for j in range(i + 1, len(puzzle)):
+            if puzzle[i] != 0 and puzzle[j] != 0 and puzzle[i] > puzzle[j]:
                 inversões += 1
-    return inversões % 2 == 0  # Retorna True se o número de inversões for par
+    return inversões % 2 == 0
 
 # Função para reiniciar o jogo para o estado inicial embaralhado
 def reiniciar_para_estado_embaralhado():
-    global puzzle
-    puzzle = estado_embaralhado_inicial  # Restaura o estado embaralhado inicial
-    update_buttons(puzzle)  # Atualiza os botões com o estado inicial
+    global puzzle, current_state
+    puzzle = estado_embaralhado_inicial
+    current_state = puzzle.copy()  # Atualiza current_state para o mesmo estado do puzzle
+    update_buttons(current_state)  # Atualiza os botões com o estado inicial
 
 # Função para reiniciar o jogo
 def reiniciar_jogo():
-    embaralhar_puzzle()  # Embaralha o quebra-cabeça
-    update_buttons(puzzle)  # Atualiza os botões com o novo estado do quebra-cabeça
+    embaralhar_puzzle()
+    update_buttons(puzzle)
 
 # Função para imprimir o histórico de movimentos
 def print_historico_movimentos():
@@ -90,41 +81,48 @@ def print_historico_movimentos():
 
 # Função para atualizar o puzzle e os botões
 def update_puzzle(row, col):
-    global puzzle
-    puzzle = mover_peça(row, col, puzzle)  # Atualiza o puzzle com o retorno da função mover_peça
-    update_buttons(puzzle)  # Atualiza os botões com o estado atual
+    global puzzle, current_state
+    next_config = mover_peça(row, col, current_state)
+
+    if solução(next_config):
+        puzzle = next_config
+        update_buttons(puzzle)
+        messagebox.showinfo("8 Puzzle", "Parabéns, você ganhou!!!")
+        estado_final = [puzzle[i:i + 3] for i in range(0, len(puzzle), 3)]
+        historico_movimentos.append((estado_final, "Puzzle resolvido"))
+        print_historico_movimentos()
+    else:
+        current_state = next_config
+        update_buttons(current_state)
 
 # Criar a interface
 root = tk.Tk()
 root.title("8 Puzzle")
-root.configure(bg="lightgray")  # Define a cor de fundo da janela principal
-buttons = []  # Lista para armazenar os botões do quebra-cabeça
+root.configure(bg="lightgray")
+buttons = []
 
-frame = tk.Frame(root, padx=10, pady=10, bg="lightgray")  # Cria um frame para os botões do quebra-cabeça com fundo lightgray
+frame = tk.Frame(root, padx=10, pady=10, bg="lightgray")
 frame.pack()
 
-# Criar botões para o quebra-cabeça
-for i in range(3):  # Iterar sobre linhas
-    for j in range(3):  # Iterar sobre colunas
-        botão = tk.Button(frame, text="", width=6, height=3,  # Cria um botão
-                        font=("Arial", 16, "bold"),
-                        bg="purple", fg="white",  # Define a cor de fundo dos botões e a cor do texto
-                        activebackground="lightgray",
-                        command=lambda row=i, col=j: update_puzzle(row, col))  # Passa o estado atual do puzzle
-        botão.grid(row=i, column=j, padx=5, pady=5)  # Adiciona o botão ao grid
-        buttons.append(botão)  # Adiciona o botão à lista de botões
+for i in range(3):
+    for j in range(3):
+        botão = tk.Button(frame, text="", width=6, height=3,
+                          font=("Arial", 16, "bold"),
+                          bg="purple", fg="white",
+                          activebackground="lightgray",
+                          command=lambda row=i, col=j: update_puzzle(row, col))
+        botão.grid(row=i, column=j, padx=5, pady=5)
+        buttons.append(botão)
 
-# Criar um frame para os botões de controle
-control_frame = tk.Frame(root, pady=10, bg="lightgray")  # Cria um frame para os botões de controle com fundo lightgray
+control_frame = tk.Frame(root, pady=10, bg="lightgray")
 control_frame.pack()
 
-# Adicionar botões de controle ao frame
-reinicia_botão = tk.Button(control_frame, text="Novo Jogo", command=reiniciar_jogo, font=("Arial", 12, "bold"), bg="purple", fg="white")  # Cria o botão de reinício
-reinicia_botão.grid(row=0, column=0, padx=5)  # Adiciona o botão ao grid
+reinicia_botão = tk.Button(control_frame, text="Novo Jogo", command=reiniciar_jogo, font=("Arial", 12, "bold"), bg="purple", fg="white")
+reinicia_botão.grid(row=0, column=0, padx=5)
 
-restaurar_estado_botão = tk.Button(control_frame, text="Reiniciar", command=reiniciar_para_estado_embaralhado, font=("Arial", 12, "bold"), bg="purple", fg="white")  # Cria o botão para restaurar estado embaralhado
-restaurar_estado_botão.grid(row=0, column=1, padx=5)  # Adiciona o botão ao grid
+restaurar_estado_botão = tk.Button(control_frame, text="Reiniciar", command=reiniciar_para_estado_embaralhado, font=("Arial", 12, "bold"), bg="purple", fg="white")
+restaurar_estado_botão.grid(row=0, column=1, padx=5)
 
-reiniciar_jogo()  # Configuração inicial do quebra-cabeça
+reiniciar_jogo()
 
-root.mainloop()  # Inicia o loop principal da interface
+root.mainloop()
